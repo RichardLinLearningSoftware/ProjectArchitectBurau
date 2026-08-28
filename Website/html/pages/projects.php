93b1c9e4e5f6e53da4627f8000dc2ea3e61de099
Richard Lin <1211675@student.roc-nijmegen.nl>
2026-08-28 12:32:52 +0200

    Script update
    -Projects are now draggable

--- Website/html/pages/projects.php
+++ Website/html/pages/projects.php
@@ -11,9 +11,9 @@
     <?php
         for($i  = 0; $i < count($result); $i++){
     ?>
-        <div class="project-box-container">
-            <img class="project-image" src="assets/images/<?php echo $result[$i]['image_second'];?>" alt="Title">
-            <img class="project-image-2" src="assets/images/<?php echo $result[$i]['image_main'];?>" alt="Title">
+        <div onmousedown="MouseDown(this)" class="project-box-container" id="<?php echo $result[$i]["id"]?>">
+            <img draggable="false" class="project-image" src="assets/images/<?php echo $result[$i]['image_second'];?>" alt="Title">
+            <img draggable="false" class="project-image-2" src="assets/images/<?php echo $result[$i]['image_main'];?>" alt="Title">
             <div class="project-info-container">
                 <h3 class="project-title">
                     <?php echo $result[$i]['title'];?>
--- Website/html/scripts/main.js
+++ Website/html/scripts/main.js
@@ -1,13 +1,32 @@
-let mouseDown = false;
-window.onmousedown = () => {
-  mouseDown = true;
-  if (mouseDown) {
-    console.log('mouse button down')
-  }
+let dragging = "none";
+let holding = false;
+let x = 0;
+let y = 0;
+
+function MouseDown(e) {
+    holding = true;
+    dragging = document.getElementById(e.id);
 }
 window.onmouseup = () => {
-  mouseDown = false;
-  if (!mouseDown) {
-    console.log('mouse button up')
-  }
+    dragging.style.transition = 0.5+"s";
+    holding = false;
+    dragging = "none";
+}
+
+document.addEventListener("mousemove", MoveCursor);
+
+function MoveCursor(e){
+    if (holding) {
+        x = e.clientX;
+        y = e.clientY;
+
+        dragging.style.zIndex  = 1;
+        dragging.style.transition = 0+"s";
+        dragging.style.position = "absolute";
+        dragging.style.top = y + "px";
+        dragging.style.left = x + "px";
+
+        x = e.clientX;
+        y = e.clientY;
+    }
 }
